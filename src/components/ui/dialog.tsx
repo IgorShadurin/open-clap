e37267c266be37ef1283135f55aa201e4261528a
Igor Shadurin <igor.shadurin@gmail.com>
2026-02-20 01:08:53 +0300

    feat: fix modal animation

--- src/components/ui/dialog.tsx
+++ src/components/ui/dialog.tsx
@@ -15,7 +15,7 @@ const DialogOverlay = React.forwardRef<
 >(({ className, ...props }, ref) => (
   <DialogPrimitive.Overlay
     className={cn(
-      "fixed inset-0 z-50 bg-black/50 data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=open]:fade-in-0",
+      "fixed inset-0 z-50 bg-black/50 transition-opacity duration-150 data-[state=open]:opacity-100 data-[state=closed]:opacity-0",
       className,
     )}
     {...props}
@@ -32,7 +32,7 @@ const DialogContent = React.forwardRef<
     <DialogOverlay />
     <DialogPrimitive.Content
       className={cn(
-        "fixed left-1/2 top-1/2 z-50 grid w-full max-w-[calc(100%-2rem)] -translate-x-1/2 -translate-y-1/2 transform-gpu gap-4 rounded-lg border border-black/20 bg-white p-6 shadow-lg outline-none duration-200 will-change-transform will-change-opacity data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=open]:fade-in-0 data-[state=closed]:zoom-out-95 data-[state=open]:zoom-in-95 sm:max-w-lg",
+        "fixed left-1/2 top-[18%] z-50 grid w-full max-w-[calc(100%-2rem)] -translate-x-1/2 gap-4 rounded-lg border border-black/20 bg-white p-6 shadow-lg outline-none transition-opacity duration-150 data-[state=open]:opacity-100 data-[state=closed]:opacity-0 sm:max-w-lg",
         className,
       )}
       ref={ref}
